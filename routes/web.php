1d447002b6689ad339329f43ebb53bae086696dc
imanudin-it <imanudin.it@gmail.com>
2023-10-06 13:19:28 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -101,6 +101,7 @@ Route::group(['middleware' => ['auth']], function ()
     Route::delete('/dashboard/informasi-umum/{id}', [DB_InformasiUmumController::class, 'destroy']);
     
     Route::resource('/dashboard/galeri', DB_GalleryController::class);
-    Route::resource('/dashboard/galeri-foto', DB_GaleriFoto::class);
+    Route::post('/dashboard/galeri-foto', DB_GaleriFoto::class, 'store');
+    Route::delete('/dashboard/galeri-foto/{id}', DB_GaleriFoto::class, 'destroy');
     
 }); //route middleware auth
